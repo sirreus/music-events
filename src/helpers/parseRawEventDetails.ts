68df7e8cbd4d574c02678d5c042a564496a7381f
Kirill Egorov <kirill.egorov@x5.ru>
2023-12-08 12:47:47 +0200

    event date format

--- src/helpers/parseRawEventDetails.ts
+++ src/helpers/parseRawEventDetails.ts
@@ -1,13 +1,17 @@
 import { IRawImage } from "../store/types";
+import moment from "moment";
 
 function parseRawEventDetails(data: any) {
   return data.map((rawData: any) => {
     const { dates, _embedded: location, images } = rawData;
 
-    const eventDate = `${dates.start.localDate} ${
-      dates.start.localTime ||
-      dates.access.startDateTime.split("T")[1].split("Z")[0]
-    }`;
+    const dayOfWeek = moment(dates.start.localDate).format("dddd");
+
+    const time = dates.start.localTime
+      ? dates.start.localTime.slice(0, -3)
+      : moment(dates.access.startDateTime).format("HH:mm");
+
+    const eventDate = `${dayOfWeek}, ${dates.start.localDate} @ ${time}`;
 
     const eventLocation = `${location?.venues[0].address.line1}, ${location?.venues[0].city.name}, ${location?.venues[0].country.name}`;
     const eventImage = images.find(
